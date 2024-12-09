ada9ef3e323d8b1dbe97d8fb697406876d08512d
abhi9262 <abhishek10kumar2002@gmail.com>
2024-12-09 23:30:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,126 @@
+/*console.log("------Welcome in JS------")
+
+const a = 5;
+        let b = 6;
+        var r = a + b; 
+        console.log(a + b);
+        console.log( "------Variable in JS------")
+
+        //var,let,const:
+
+        //var:
+        var var1=5;
+        var var2=" Variable-2-in-string-format";
+        var var3= 8453628292030;
+
+        var1 = " hello";
+        var var1 = ""
+        //let:
+        let let1=5;
+        let let2=" Variable-2-in-string-format";
+        let let3= 8453628292030;
+
+        let1 = " hello";
+
+        //cont :
+        const cons1=5;
+        const const2=" Variable-2-in-string-format";
+        const const3= 8453628292030;
+        
+
+
+
+        //print sum of 5 numbers:
+        const num1 =5;
+        numb2 =6;
+        numb3 =7;
+        numb4 =8;
+        numb5 =99;
+        let sum = num1+numb2+numb3+numb4+numb5;
+        console.log(" The sum of the numbers is:"+sum);
+
+        //print average of numbers:
+        const num1 =5;
+        numb2 =6;
+        numb3 =7;
+        numb4 =8;
+        numb5 =99;
+        let average = sum/5;
+        console.log(" The average of the numbers is:"+average);
+        //print a bigg number:
+        const num1=5;
+        const num2=6;
+        const num3=7;
+        
+        if(num1>num2 && num1>num3){
+            console.log("num1",num1);
+        }
+        else if( num2>num3 && num2>num1){
+            console.log("num2",num2);
+        }
+        else{
+            console.log("num1",num3);
+}
+
+
+
+
+
+
+        //loops:
+        for( let i=1; i<10; i++) {
+            const tableLine = "9 * " + i + " = " +9* i;
+            console.log(tableLine);
+        }
+        // print all values :[dvisible by 3]
+        
+        // Sample array of numbers
+        for(let i=0;i<100; i++)
+  if (i % 3 === 0) {
+    console.log(i);
+  }
+  // divisible by 3&5
+  for (let i = 1; i < 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(i);
+    }
+  }*/
+  // divisible by 3&5&9
+ /*for (let i = 1; i < 100; i++) {
+    if (i % 3 === 0 && i%5==0 && i%9==0)
+      console.log(i);
+    }*/
+   /* var n=10;
+    for( let i=1; i<10; i++) {
+      const tableLine = n +"* " + i + " = " +n* i;
+      console.log(tableLine);
+  }*/
+    /*  const num1=5;
+      const num2=6;
+      const num3=7;
+      const num4= 8;
+      
+      if(num1>num2 && num1>num3 && num1>num4) {
+          console.log("num1",num1);
+      }
+      else if( num2>num3 && num2>num1 && num2>num4){
+          console.log("num2",num2);
+      }
+      else if(num3>num1 && num3>num2 && num3 > num4){
+          console.log("num1",num3);
+        }
+      else{
+        console.log("num4",num4);
+      
+        
+
+ //loops:
+ for( let i=n; n>1; i++) {
+  const reverse = "n+" + i + " = " +n i;
+  console.log(reverse);}*/
+  
+  //assume user enter number N=?
+// Reverse count from 10 to 1
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
